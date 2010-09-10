31bd375da417442d61aebf75bec9843a50f4e264
Benny Tsai <benny.tsai@gmail.com>
2010-09-10 01:12:59 -0600

    changed to use GregorianCalendar

--- clojure/p19.clj
+++ clojure/p19.clj
@@ -1,15 +1,24 @@
 (ns p19
   (:use [utils :only (timed-test)])
-  (:import [java.util Date]))
+  (:import [java.util GregorianCalendar]))
+
+(defn inc-date [d]
+  (let [new-d (.clone d)]
+    (.add new-d (. GregorianCalendar DATE) 1)
+    new-d))
+
+(defn day-of-week [d] (.get d (. GregorianCalendar DAY_OF_WEEK)))
+
+(defn day-of-month [d] (.get d (. GregorianCalendar DAY_OF_MONTH)))
+
+(defn sunday-on-first? [d]
+  (and (= (day-of-week d) (. GregorianCalendar SUNDAY))
+       (= (day-of-month d) 1)))
 
 (timed-test
  "Problem 19"
  171
- (let [start (Date. 1 0 1)
-       end (Date. 101 0 1)
-       millis-per-day (* 24 60 60 1000)
-       times (range (.getTime start) (.getTime end) millis-per-day)
-       dates (map #(Date. %) times)
-       sunday-on-first? #(and (zero? (.getDay %))
-			      (= 1 (.getDate %)))]
+ (let [start (GregorianCalendar. 1901 0 1)
+       end (GregorianCalendar. 2001 0 1)
+       dates (take-while #(not= end %) (iterate inc-date start))]
    (count (filter sunday-on-first? dates))))
