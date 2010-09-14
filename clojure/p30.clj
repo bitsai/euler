f848464dfdc5fe7b5a1daeb44a9d2d38afa42311
Benny Tsai <benny.tsai@gmail.com>
2010-09-14 11:13:37 -0600

    solved problem 30 in clojure

---
+++ clojure/p30.clj
@@ -0,0 +1,14 @@
+(ns p30
+  (:use [utils :only (digits sum timed-test)])
+  (:use [clojure.contrib.math :only (expt)]))
+
+(defn sum-of-5th-powers? [n]
+  (let [fifth-powers (map #(expt % 5) (digits n))]
+    (= n (sum fifth-powers))))
+
+(timed-test
+ "Problem 30"
+ 443839
+ (let [max-n (* 6 (expt 9 5))
+       nums (filter sum-of-5th-powers? (range 2 (inc max-n)))]
+   (sum nums)))
