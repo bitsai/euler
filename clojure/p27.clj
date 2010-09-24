5d463bd58ab0e0982bca302e34bd56f535163a9c
Benny Tsai <benny.tsai@gmail.com>
2010-09-24 17:14:15 -0600

    refactoring for clarity

--- clojure/p27.clj
+++ clojure/p27.clj
@@ -1,16 +1,14 @@
 (ns p27
-  (:use [utils :only (quadratic prime? natural-nums timed-test)])
+  (:use [utils :only (natural-nums quadratic prime? timed-test)])
   (:use [clojure.contrib.lazy-seqs :only (primes)]))
 
-(defn prime-quadratic? [n a b] (prime? (quadratic n a b)))
-
-(defn count-consecutive-primes [a b]
-  (count (take-while #(prime-quadratic? % a b) (natural-nums))))
+(defn consecutive-primes [a b]
+  (take-while #(prime? (quadratic % a b)) (natural-nums)))
 
 (timed-test
  "Problem 27"
  -59231
  (let [pairs (for [a (range -999 1000 2)
 		   b (take-while #(< % 1000) primes)]
-	       [(* a b) (count-consecutive-primes a b)])]
+	       [(* a b) (count (consecutive-primes a b))])]
    (first (apply max-key second pairs))))
