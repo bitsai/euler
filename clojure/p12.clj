268a9246ea38288c29981f0da73e592714d028eb
Benny Tsai <benny.tsai@gmail.com>
2010-10-15 23:41:35 -0600

    dramatically sped up problem 12 solution

--- clojure/p12.clj
+++ clojure/p12.clj
@@ -1,8 +1,17 @@
 (ns p12
-  (:use [utils :only (factors triangle-nums timed-test)]))
+  (:use [utils :only (factors triangle-nums whole-nums timed-test)]))
+
+(defn count-triangle-num-factors [n]
+  (let [D #(count (factors %))]
+    (cond
+     (even? n) (* (D (/ n 2)) (D (inc n)))
+     :else (* (D n) (D (/ (inc n) 2))))))
+
+(defn lte-500-factors? [[t n]]
+  (<= (count-triangle-num-factors n) 500))
 
 (timed-test
  "Problem 12"
  76576500
- (let [lte-500-factors? #(<= (count (factors %)) 500)]
-   (first (drop-while lte-500-factors? (triangle-nums)))))
+ (let [pairs (map vector (triangle-nums) (whole-nums))]
+   (ffirst (drop-while lte-500-factors? pairs))))
