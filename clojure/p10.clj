f6b14af5b2546b91393198cef0e34b084a77ff41
Benny Tsai <benny.tsai@gmail.com>
2011-01-29 17:10:48 -0700

    speed up solution 10 using parallelism and Sieve of Eratosthenes

--- clojure/p10.clj
+++ clojure/p10.clj
@@ -1,9 +1,24 @@
 (ns p10
-  (:use [utils :only (sum timed-test)])
-  (:use [clojure.contrib.lazy-seqs :only (primes)]))
+  (:use [utils :only (multiple? timed-test)])
+  (:use [clojure.contrib.generic.math-functions :only (sqr sqrt)]))
+
+(defn sieve [n]
+  (loop [[p & _ :as nums] (cons 2 (range 3 (inc n) 2))
+	 primes []]
+    (cond
+     (< n 2) []
+     (> (sqr p) n) (concat primes nums)
+     :else (recur (remove #(multiple? % p) nums)
+		  (conj primes p)))))
 
 (timed-test
  "Problem 10"
  142913828922
- (let [lte-2-mil? #(<= % 2000000)]
-   (sum (take-while lte-2-mil? primes))))
+ (let [prime-divisors (sieve (sqrt 2000000))
+       not-prime? (fn [x] (some #(multiple? x %) prime-divisors))
+       f (fn [nums] (reduce + (remove not-prime? nums)))]
+   (+ (reduce + prime-divisors)
+      (reduce + (pmap f [(range 3 499999 2)
+			 (range 499999 999999 2)
+			 (range 999999 1499999 2)
+			 (range 1499999 2000000 2)])))))
