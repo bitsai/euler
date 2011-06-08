a15dfa12c33ad3c6a2ec61ca6acac7792a758fd0
Benny Tsai <benny.tsai@gmail.com>
2011-06-08 10:28:08 -0600

    for problem 12, once we know n, we can directly compute the nth triangle number instead of grabbing the nth item from the sequence of triangle numbers

--- clojure/p12.clj
+++ clojure/p12.clj
@@ -1,14 +1,19 @@
 (ns p12
-  (:use [utils :only (factors triangle-nums timed-test)]))
+  (:use [clojure.contrib.seq :only (find-first)])
+  (:use [utils :only (factors timed-test)]))
 
-(defn count-triangle-factors [n]
-  (let [D #(count (factors %))]
-    (cond
-     (even? n) (* (D (/ n 2)) (D (inc n)))
-     :else (* (D n) (D (/ (inc n) 2))))))
+(defn nth-triangle-num [n]
+  (* n (/ (inc n) 2)))
+
+;; From Project Euler solution pdf
+(defn count-nth-triangle-num-factors [n]
+  (let [D (fn [x] (count (factors x)))]
+    (if (even? n)
+      (* (D (/ n 2)) (D (inc n)))
+      (* (D n) (D (/ (inc n) 2))))))
 
 (timed-test
  "Problem 12"
  76576500
- (let [n (first (filter #(> (count-triangle-factors %) 500) (range)))]
-   (nth (triangle-nums) (dec n))))
+ (let [n (find-first #(> (count-nth-triangle-num-factors %) 500) (range))]
+   (nth-triangle-num n)))
