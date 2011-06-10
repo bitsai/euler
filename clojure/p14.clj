ff5057c63dcab8c3685ecd6048116afb7c2edc5d
Benny Tsai <benny.tsai@gmail.com>
2011-06-10 10:23:19 -0600

    speed up Clojure solution 14 via manual memoization

--- clojure/p14.clj
+++ clojure/p14.clj
@@ -1,17 +1,28 @@
 (ns p14
   (:use [euler :only (max-key timed-test)]))
 
+(def N 1000000)
+
+(def table (int-array N 0))
+
+(defn memoized? [n]
+  (and (< n N) (pos? (aget ^ints table n))))
+
+(defn collatz [n]
+  (if (bit-test n 0)
+    (inc (* 3 n))
+    (/ n 2)))
+
 (defn count-terms [n]
-  (loop [n n
-	 count 1]
-    (if (= n 1)
-      count
-      (recur (if (bit-test n 0)
-               (inc (* 3 n))
-               (bit-shift-right n 1))
-             (inc count)))))
+  (cond
+   (= n 1) 1
+   (memoized? n) (aget ^ints table n)
+   :else (let [cnt (inc (count-terms (collatz n)))]
+           (if (< n N)
+             (aset ^ints table n (int cnt)))
+           cnt)))
 
 (timed-test
  "Problem 14"
  837799
- (apply max-key count-terms (range 1 1000000)))
+ (apply max-key count-terms (range 1 N)))
